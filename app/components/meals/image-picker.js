be699d1c9d480d8c9ed87f3c21a2f2ee03d92a0c
Gonzalo Alvarez <gonzaloralvarezcampos@gmail.com>
2025-05-20 17:23:35 -0300

    image save action and query added

--- app/components/meals/image-picker.js
+++ app/components/meals/image-picker.js
@@ -1,40 +1,45 @@
-"use client";
-import { useRef, useState } from "react";
-import Image from "next/image";
-import classes from "./image-picker.module.css";
+'use client';
+
+import { useRef, useState } from 'react';
+import Image from 'next/image';
+
+import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
-  const [pickedImage, setPickedImage] = useState("");
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
 
-  const handlePickImage = (event) => {
+  function handlePickClick() {
     imageInput.current.click();
-    const file = event.target.files[0];
-    if (!file) {
-        setPickedImage(null);
-        return;
-    }
-    const reader = new FileReader();
+  }
 
-    setPickedImage(reader.result);
-    
-    reader.onload = () => {
-        setPickedImage(reader.result);
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
     };
-    reader.readAsDataURL(file);
-  };
+
+    fileReader.readAsDataURL(file);
+  }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
         <div className={classes.preview}>
-          {!pickedImage && <p>No image picked yet!</p>}
+          {!pickedImage && <p>No image picked yet.</p>}
           {pickedImage && (
             <Image
               src={pickedImage}
-              alt="Meal Image"
-              className={classes.image}
+              alt="The image selected by the user."
               fill
             />
           )}
@@ -43,18 +48,17 @@ export default function ImagePicker({ label, name }) {
           className={classes.input}
           type="file"
           id={name}
-          name={name}
           accept="image/png, image/jpeg"
-          required
+          name={name}
           ref={imageInput}
-          onChange={handlePickImage}
+          onChange={handleImageChange}
         />
         <button
-          type="button"
           className={classes.button}
-          onClick={handlePickImage}
+          type="button"
+          onClick={handlePickClick}
         >
-          Select Image
+          Pick an Image
         </button>
       </div>
     </div>
